acfe0b6dadda76cd343e7e138ef6d1ae827d885d
Jesse Nwajiaku <120261066+kumja1@users.noreply.github.com>
2023-11-03 01:06:52 +0000

    Update 3 files

--- GaiaDimensions_BP/scripts/portal_utils.js
+++ GaiaDimensions_BP/scripts/portal_utils.js
@@ -1,4 +1,4 @@
-import { Vector, BlockPermutation } from "@minecraft/server"
+import { Vector, BlockPermutation, world } from "@minecraft/server"
 import { log, vectorToString } from './utils.js'
 
 export function isUnlitPortal(corner, dimension, x_oriented){
@@ -63,19 +63,56 @@ export function breakPortal(corner, dimension, x_oriented){
 }
 
 
+
+
+
 export function decode(input) {
-  const L = {
-    a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8, j: 9,
-    k: 10, l: 11, m: 12, n: 13, o: 14, p: 15, q: 16, r: 17, s: 18, t: 19,
-    u: 20, v: 21, w: 22, x: 23, y: 24, z: 25, A: 26, B: 27, C: 28, D: 29,
-    E: 30, F: 31, G: 32, H: 33, I: 34, J: 35, K: 36, L: 37, M: 38, N: 39,
-    O: 40, P: 41, Q: 41, R: 42, S: 43, T: 44, U: 45, V: 46, W: 47, X: 48,
-    Y: 49, Z: 50
-  };
+    const L = {
+      a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8, j: 9,
+      k: 10, l: 11, m: 12, n: 13, o: 14, p: 15, q: 16, r: 17, s: 18, t: 19,
+      u: 20, v: 21, w: 22, x: 23, y: 24, z: 25, A: 26, B: 27, C: 28, D: 29,
+      E: 30, F: 31, G: 32, H: 33, I: 34, J: 35, K: 36, L: 37, M: 38, N: 39,
+      O: 40, P: 41, Q: 41, R: 42, S: 43, T: 44, U: 45, V: 46, W: 47, X: 48,
+      Y: 49, Z: 50
+    };
+  
+    const P = input.split('*');
+    const V = P.map((p) => p.split('').reduce((a, l) => a * L[l], 1));
+    const C = V.reduce((a, v) => a * v, 1); 
+  
+    return C;
+  }
 
-  const P = input.split('*');
-  const V = P.map((p) => p.split('').reduce((a, l) => a * L[l], 1));
-  const C = V.reduce((a, v) => a * v, 1); 
+  export function ConvertCoords(location, fromDimension, toDimension) {
+    const scaleFactor = 1 / 4; // 1 block in 'gaia' is 4 blocks in 'overworld'
+    const xOffset = 300000; // Offset for the x-axis
+    const zOffset = 300000; // Offset for the z-axis
 
-  return C;
+    switch (fromDimension) {
+        case 'minecraft:overworld':
+            switch (toDimension) {
+                case 'gaia:gaia':
+                    return {
+                        x: Math.floor((location.x + xOffset) * scaleFactor),
+                        y: location.y,
+                        z: Math.floor((location.z + zOffset) * scaleFactor)
+                    };
+                default:
+                    throw new Error(`Unsupported conversion to ${toDimension}`);
+            }
+        case 'gaia:gaia':
+            switch (toDimension) {
+                case 'minecraft:overworld':
+                    return {
+                        x: Math.floor((location.x - xOffset) / scaleFactor ),
+                        y: location.y,
+                        z: Math.floor((location.z - xOffset) / scaleFactor)
+                    };
+                default:
+                    throw new Error(`Unsupported conversion to ${toDimension}`);
+            }
+        default:
+            throw new Error(`Unsupported conversion from ${fromDimension}`);
+    }
 }
+
