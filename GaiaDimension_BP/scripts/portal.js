d4d42d45f047fefce491c465a54ad2ebecdc5ce4
kumja1 <nwajiakumj@gmail.com>
2023-11-05 20:22:04 -0500

    Made some changes to portal placement(Now works!) Now teleports the player to the overworld based off the converted coordinates of the coords indicator

---
+++ GaiaDimension_BP/scripts/portal.js
@@ -0,0 +1,142 @@
+import { world, system, Vector, Entity, Dimension, Block, World, Player,Container } from "@minecraft/server"
+import {log, inGaiaDimension, delay } from './utils.js'
+import { placePortal,decode,ConvertCoords, breakPortal} from './portal_utils.js'
+const locMap = new Map()
+const dimensions = ['overworld','nether','the_end'].map(dimensionStr=>world.getDimension(dimensionStr))
+const overworld = dimensions.find(d=>d.id === 'minecraft:overworld')
+const the_end = dimensions.find(d=>d.id === 'minecraft:the_end')
+Entity.prototype.isInPortal = function (){
+    try {
+	return this.dimension.getBlock(this.location).typeId === "gaia:gaia_portal";
+    } catch (e){}
+};
+
+function getTopBlock(location, dimension){
+    let loc = new Vector(Math.floor(location.x), 310, Math.floor(location.z))
+    return Vector.add(dimension.getBlockFromRay(loc, new Vector(0,-1,0)).block.location, new Vector(0,1,0))
+}
+
+async function tpToGaia(entity){
+    if (!inGaiaDimension(entity)&& entity.dimension.id === 'minecraft:the_end'){
+        log(`Sending ${entity.nameTag} back to the End`)
+        entity.teleport({x:0,y:65,z:0},{dimension:the_end})
+        return;
+            }
+            entity.teleport(ConvertCoords(new Vector(entity.location.x,entity.location.y,entity.location.z),'minecraft:overworld','gaia:gaia'), {dimension: the_end})
+            await delay(0.8)
+        placePortal(new Vector(entity.location.x,entity.location.y,entity.location.z), the_end, true)
+    await delay(0.8)
+    entity.teleport(getTopBlock(entity.location,entity.dimension),{dimension:entity.dimension})
+    log(entity.typeId + " sent to Gaia Dimension")
+
+  
+}
+
+
+
+/**
+ * 
+ * @param {number} x 
+ */
+function MathRound (x) {
+  return Math.round(x * 1000) / 1000;
+};
+
+/**
+ * Returns `true` if entity is movings
+ * @param {Entity} entity 
+ */
+function isMoving (entity) {
+  if (!(entity instanceof Player) && !(entity instanceof Entity)) throw new TypeError('Parameter is not Entity or Player');
+  
+  /**
+   * @type {import("@minecraft/server").Vector3}
+   */
+  const vector = {
+    x: MathRound(entity.getVelocity().x),
+    y: MathRound(entity.getVelocity().y),
+    z: MathRound(entity.getVelocity().z)
+  };
+
+  if (vector.x === 0 && vector.y === 0 && vector.z === 0) return false;
+  else return true;
+};;
+
+export default isMoving;
+ 
+async function backToDimension(entity,coord){
+    try{
+    if (entity.typeId == "minecraft:player"){
+        const teleport = coord
+        let dimension = overworld  ?? entity.getSpawnPoint().dimension
+        entity.teleport(ConvertCoords({x:teleport.x+2,y:teleport.y,z:teleport.z+2},'gaia:gaia','minecraft:overworld'), {dimension:dimension})
+        await delay(1);
+        entity.teleport(ConvertCoords(getTopBlock({x:teleport.x,y:teleport.y,z:teleport.z}, overworld),'gaia:gaia','minecraft:overworld'), {dimension: dimension})
+    } else {
+        entity.teleport(world.getDefaultSpawnLocation(), {dimension: overworld})
+        await delay(1);
+        entity.teleport(getTopBlock(world.getDefaultSpawnLocation(), overworld), {dimension: overworld})
+    }
+    log(entity.typeId + " sent to overworld")
+} catch (e) {
+}
+}
+
+const prevLocationMap = new Map();
+
+system.runInterval(() => {
+    dimensions.forEach(dimension => {
+        dimension.getEntities().forEach(entity => {
+            const lastInPortal = entity.hasTag("inPortal");
+            const inPortal = entity.isInPortal() || (dimension.getBlock(new Vector(entity.location.x, 0, entity.location.z)) === undefined && lastInPortal);
+            const currentLocation = entity.location;
+            inPortal ? entity.addTag('inPortal') : entity.removeTag('inPortal');
+            if (entity.typeId === 'minecraft:player') {  
+                const isPlayerMoving = isMoving(entity);
+                if (isPlayerMoving) {
+                    const prevLocation = prevLocationMap?.get(entity.nameTag);
+                        const deltaX = Math.floor(currentLocation.x - prevLocation?.x);
+                        const deltaZ = Math.floor(currentLocation.z - prevLocation?.z);
+
+                        let loc = entity.location;
+                        const calVector = new Vector(Math.floor(((loc.x - 200000) / 1000) + deltaX), loc.y, Math.floor(((loc.z - 200000) / 1000) + deltaZ));
+
+                        if (!locMap.has(entity.nameTag)) {
+                            locMap.set(entity.nameTag, calVector);
+                            delete loc.x;
+                            delete loc.y;
+                            delete loc.z;
+                        }
+
+                        if (deltaX > 0 || deltaZ > 0) {
+                            locMap.set(entity.nameTag, new Vector(locMap?.get(entity.nameTag)?.x + 1, locMap?.get(entity.nameTag)?.y, locMap?.get(entity.nameTag)?.z + 1)); // Add 1 to the movement when moving forward
+                        } else if (deltaX < 0 || deltaZ < 0) {
+                            locMap.set(entity.nameTag, new Vector(locMap?.get(entity.nameTag)?.x - 1, locMap?.get(entity.nameTag)?.y, locMap?.get(entity.nameTag)?.z - 1)); // Subtract 1 from the movement when moving backward
+                        }
+
+                      const coord = `x:${locMap?.get(entity.nameTag)?.x} y:${Math.round(entity.location.y)} z:${locMap?.get(entity.nameTag)?.z}`
+                        inGaiaDimension(entity) ? entity?.onScreenDisplay?.setActionBar(coord): undefined;
+                        if (inPortal && !lastInPortal) {
+                            inGaiaDimension(entity) ? backToDimension(entity,parseCoords(coord)) : tpToGaia(entity);
+                        }
+                       
+                    }
+                prevLocationMap.set(entity.nameTag, currentLocation);
+            }
+        });
+    });
+}, 6);
+
+function parseCoords(coord){
+return {
+    x: parseInt(coord.split(':')[1]),
+    y:parseInt(coord.split(':')[2]),
+    z: parseInt(coord.split(':')[3])
+};
+}
+
+
+world.afterEvents.playerBreakBlock.subscribe(ev=>{
+    const {block} = ev
+    breakPortal(block.location,block.dimension,true)
+},{blockTypes:['gaia:gaia_portal']})
---
+++ GaiaDimension_BP/scripts/portal_utils.js
@@ -0,0 +1,147 @@
+import { Vector, BlockPermutation,Dimension,system,Block } from "@minecraft/server"
+import { log, vectorToString } from './utils.js'
+
+export function isUnlitPortal(corner, dimension, x_oriented){
+    let isValid = true
+    for (let x = 0; x < 4; x++){
+        for (let y = 0; y < 5; y++){
+            let blockpos = Vector.add(corner, new Vector(x_oriented ? 0 : x, y, x_oriented ? x : 0));
+            let blocktype = dimension.getBlock(blockpos).typeId
+            let is_edge = x == 0 || y == 0 || x == 3 || y == 4
+            if (is_edge && blocktype != "gaia:keystone_block"){
+                isValid = false
+                break
+            }
+            if (!is_edge && blocktype != "minecraft:air"){
+                isValid = false
+                break
+            }
+        }
+    }
+    return isValid
+}
+
+export function isLitPortal(corner, dimension, x_oriented){
+    let isValid = true
+    for (let x = 0; x < 4; x++){
+        for (let y = 0; y < 5; y++){
+            let blockpos = Vector.add(corner, new Vector(x_oriented ? 0 : x, y, x_oriented ? x : 0));
+            let blocktype = dimension.getBlock(blockpos).typeId
+            let is_edge = x == 0 || y == 0 || x == 3 || y == 4
+            if (is_edge && blocktype != "gaia:keystone_block"){
+                isValid = false
+                break
+            }
+            if (!is_edge && blocktype != "gaia:gaia_portal"){
+                isValid = false
+                break
+            }
+        }
+    }
+    return isValid
+}
+
+
+/**
+ * @param {Vector} location
+ * @param {Dimension} dimension
+ * @returns {Block}
+ */
+export function findGround(location,dimension){
+    try {
+        let blockFound;
+    let y = location.y
+    let check = system.runInterval(()=>{
+        if (y <= 0){
+            return;
+        }
+        const block = dimension?.getBlock({x:location.x,y:Math.round(y),z:location.z})
+        if (block && block.type.id !== 'minecraft:air'){
+            blockFound = block
+            system.clearRun(check)
+        } else{
+        y--
+    }
+    })
+    return blockFound
+} catch (e){console.error(e)}
+}
+export function placePortal(corner, dimension, x_oriented){
+    for (let x = 0; x < 4; x++){
+        for (let y = 0; y < 5; y++){
+            let blockpos = Vector.add(corner, new Vector(x_oriented ? 0 : x, y, x_oriented ? x : 0));
+            let is_edge = x == 0 || y == 0 || x == 3 || y == 4
+            if (is_edge){
+                dimension.getBlock(blockpos).setPermutation(BlockPermutation.resolve("gaia:keystone_block"))
+            } else {
+                dimension.getBlock(blockpos).setPermutation(BlockPermutation.resolve("gaia:gaia_portal", {"gaia:x_oriented": x_oriented}))
+            }
+        }
+    }
+}
+
+export function breakPortal(corner, dimension, x_oriented){
+    for (let x = -1; x <= 2; x++){
+        for (let y = -1; y <= 2; y++){
+            let blockpos = Vector.add(corner, new Vector(x_oriented ? 0 : x, y, x_oriented ? x : 0));
+            let block = dimension.getBlock(blockpos);
+            if (block.type.id === 'gaia:gaia_portal'){
+                block.setPermutation(BlockPermutation.resolve("minecraft:air"));
+            }
+        }
+    }
+}
+
+export function decode(input) {
+  const L = {
+    a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8, j: 9,
+    k: 10, l: 11, m: 12, n: 13, o: 14, p: 15, q: 16, r: 17, s: 18, t: 19,
+    u: 20, v: 21, w: 22, x: 23, y: 24, z: 25, A: 26, B: 27, C: 28, D: 29,
+    E: 30, F: 31, G: 32, H: 33, I: 34, J: 35, K: 36, L: 37, M: 38, N: 39,
+    O: 40, P: 41, Q: 41, R: 42, S: 43, T: 44, U: 45, V: 46, W: 47, X: 48,
+    Y: 49, Z: 50
+  };
+
+  const P = input.split('*');
+  const V = P.map((p) => p.split('').reduce((a, l) => a * L[l], 1));
+  const C = V.reduce((a, v) => a * v, 1); 
+
+  return C;
+}
+
+
+export function ConvertCoords(location, fromDimension, toDimension) {
+    const scaleFactor = 1 / 4; // 1 block in 'gaia' is 4 blocks in 'overworld'
+    const xOffset = 300000; // Offset for the x-axis
+    const zOffset = 300000; // Offset for the z-axis
+
+    switch (fromDimension) {
+        case 'minecraft:overworld':
+            switch (toDimension) {
+                case 'gaia:gaia':
+                    return {
+                        x: Math.floor((location.x + xOffset) * scaleFactor+200000),
+                        y: location.y,
+                        z: Math.floor((location.z + zOffset) * scaleFactor+200000)
+                    };
+                default:
+                    throw new Error(`Unsupported conversion to ${toDimension}`);
+            }
+        case 'gaia:gaia':
+            switch (toDimension) {
+                case 'minecraft:overworld':
+                    return {
+                        x: Math.floor(location.x / scaleFactor),
+                        y: location.y,
+                        z: Math.floor(location.z / scaleFactor)
+                    };
+                default:
+                    throw new Error(`Unsupported conversion to ${toDimension}`);
+            }
+        default:
+            throw new Error(`Unsupported conversion from ${fromDimension}`);
+    }
+}
+
+
+
