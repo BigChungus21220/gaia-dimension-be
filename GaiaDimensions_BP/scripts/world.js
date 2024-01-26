ab29d8510a7fa8b3b5d7384c96feda1a546f673e
BigChungus21220 <109574168+BigChungus21220@users.noreply.github.com>
2024-01-26 14:29:59 -0800

    removed duplicate file

--- GaiaDimensions_BP/scripts/world.js
+++ GaiaDimensions_BP/scripts/world.js
@@ -2,7 +2,7 @@ import './geyser.js';
 import './clearMobs.js';
 import './portal.js';
 import './light_portal.js';
-import './APIAdditions.js';
+import '.classMixins.js';
 import "./blocks/place.js";
 import {Gaia} from './api/Dimension.js'
 import {furnacesLoad} from "./api/furnaceAPI.js";
