95db6afd82e0477906443dc0b0f3649c8a49904d
Jesse Nwajiaku <nwajiakumj@gmail.com>
2024-02-22 19:08:03 +0000

    Some changes

--- GaiaDimensions_BP/scripts/light_portal.js
+++ GaiaDimensions_BP/scripts/light_portal.js
@@ -1,22 +1,23 @@
-import { world, Vector} from "@minecraft/server";
+import { world, Vector } from "@minecraft/server";
 import Portal from './api/Portal'
-world.afterEvents.itemUseOn.subscribe(({source,itemStack,block,blockFace}) => {
-    try {
-        if (itemStack.typeId === "gaia:glint_and_gold" && block.typeId === 'gaia:keystone_block') {
-            const pos = Vector.add(Vector.convertDirection(blockFace), block.location);
-            const lit = Portal.canLight(block.dimension.getBlock(pos));
-            if (lit) {
-                source.playSound('block.end_portal.spawn', { location: block.location });
-            }
-        }
-    } catch (error) {
 
+
+world.afterEvents.itemUseOn.subscribe(({ source, itemStack, block, blockFace }) => {
+  try {
+    if (itemStack.typeId === "gaia:glint_and_gold" && block.typeId === 'gaia:keystone_block') {
+      const pos = Vector.add(Vector.convertDirection(blockFace), block.location);
+      const lit = Portal.canLight(block.dimension.getBlock(pos));
+      if (lit) {
+        source.playSound('block.end_portal.spawn', { location: block.location });
+      }
     }
-  });
-  
-  system.afterEvents.playerBreakBlock.subscribe((ev) => {
-    const { block, player } = ev;
-    Portal.breakPortal(block.location, block.dimension, true);
-    player.playSound('break.amethyst_block', { location: block.location });
+  } catch (error) {
+
+  }
+});
+
+world.afterEvents.playerBreakBlock.subscribe(({block,player}) => {
+  Portal.breakPortal(block.location, block.dimension, true);
+  player.playSound('break.amethyst_block', { location: block.location });
 }, { blockTypes: ['gaia:keystone_block', 'gaia:gaia_portal'] });
 
