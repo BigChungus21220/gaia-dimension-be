a85628d4e910cb52faefb7a99abc70da07cdd893
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2024-02-22 20:02:24 +0700

    this works /?

--- GaiaDimensions_BP/scripts/light_portal.js
+++ GaiaDimensions_BP/scripts/light_portal.js
@@ -1,38 +1,35 @@
 import { world, Vector} from "@minecraft/server";
 import gaia from './world';
 
-world.afterEvents.itemUseOn.subscribe(async (event) => {
-    try {
-        if (event.itemStack.typeId === "gaia:glint_and_gold" && event.block.typeId === 'gaia:keystone_block') {
-            const pos = Vector.add(Vector.convertDirection(event.blockFace), event.block.location);
-
-            const portalData = {
-                location: { x: pos.x, y: pos.y, z: pos.z },
-                dimension: event.block.dimension,
-                source: event.source
-            };
-            gaia.triggerEvent('portalActivate', portalData, 'BeforeEvent');
-
-            const data = await gaia.listenFor('portalActivate', 'Canceled', 'BeforeEvent');
-            if (data && data.cancel) {
-                return;
-            }
-
-            const lit = gaia.canLight(event.block.dimension.getBlock(pos));
-
-            if (lit) {
-                gaia.triggerEvent('portalActivate', portalData, 'AfterEvent');
-                event.source.playSound('block.end_portal.spawn', { location: event.block.location });
-            }
-        }
-    } catch (error) {
-
+system.afterEvents.itemUseOn.subscribe(async (event) => {
+    if (event.itemStack.typeId === "gaia:glint_and_gold" && event.block.typeId === 'gaia:keystone_block') {
+      const pos = Vector.add(Vector.convertDirection(event.blockFace), event.block.location);
+  
+      const portalData = {
+        location: { x: pos.x, y: pos.y, z: pos.z },
+        dimension: event.block.dimension,
+        source: event.source
+      };
+  
+      gaia.triggerEvent('portalActivate', portalData, 'BeforeEvent');
+  
+      const data = await gaia.listenFor('portalActivate', 'Canceled', 'BeforeEvent');
+      if (data?.cancel) {
+        return;
+      }
+  
+      if (gaia.canLight(event.block.dimension.getBlock(pos))) {
+        gaia.triggerEvent('portalActivate', portalData, 'AfterEvent');
+        event.source.playSound('block.end_portal.spawn', { location: event.block.location });
+      }
     }
-});
-
-world.afterEvents.playerBreakBlock.subscribe((ev) => {
+  });
+  
+  system.afterEvents.playerBreakBlock.subscribe((ev) => {
     const { block, player } = ev;
-    gaia.breakPortal(block.location, block.dimension, true);
-    player.playSound('break.amethyst_block', { location: block.location });
-}, { blockTypes: ['gaia:keystone_block', 'gaia:gaia_portal'] });
-
+    if (['gaia:keystone_block', 'gaia:gaia_portal'].includes(block.typeId)) {
+      gaia.breakPortal(block.location, block.dimension, true);
+      player.playSound('break.amethyst_block', { location: block.location });
+    }
+  });
+  
