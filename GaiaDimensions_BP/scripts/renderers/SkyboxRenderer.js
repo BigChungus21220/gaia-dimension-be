aaac8f06cb86a5661bbd9058efde7ef99d054f2e
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2024-04-15 08:33:28 +0700

    Update SkyboxRenderer.js

--- GaiaDimensions_BP/scripts/renderers/SkyboxRenderer.js
+++ GaiaDimensions_BP/scripts/renderers/SkyboxRenderer.js
@@ -4,8 +4,9 @@ import Gaia from '../world/Gaia'
 import {vec3} from './vec3.js'
 
 //event that handles the skybox
-Events.playerChangeBlock.subscribe((eventData) => {
-    let spawn_pos = (eventData.player)
+Events.playerChangeBlock.subscribe((player) => {
+    let spawn_pos = (player)
+    let playerLocations = {};
     const players = Gaia.getPlayers();
     for (const player of players) {
       const { x, y, z } = player.location;
@@ -14,17 +15,29 @@ Events.playerChangeBlock.subscribe((eventData) => {
         Events.playerChangeBlock.trigger({ player: player });
       }
       const check = Gaia.isInGaia(player.location.getBlock(pos));
-      Gaia.spawnParticle("gaia:sky1", spawn_pos);
-      Gaia.spawnParticle("gaia:sky2", spawn_pos);
-      Gaia.spawnParticle("gaia:sky_side1", spawn_pos);
-      Gaia.spawnParticle("gaia:sky_side2", spawn_pos);
-      Gaia.spawnParticle("gaia:sky_side3", spawn_pos);
-      Gaia.spawnParticle("gaia:sky_side4", spawn_pos);
-      Gaia.spawnParticle("gaia:gaia_planet", spawn_pos);
+      SkyboxRenderer.spawnSkybox(player.location)
     }
   });
 
  class SkyboxRenderer {
      constructor(player) {
-         this.tick(8)
+         this.tick(59)
      }
+       /**
+   * Spawns the Skybox (in Gaia)
+   * @param {ItemStack} itemStack Particle to spawn
+   * @param {Vec3} location Location to spawn particle
+   * @param {MolangVariableMap} molangVariables Optional variables for this particle
+   */
+  static spawnSkybox( location, molangVariables = {}) {
+    Gaia.spawnParticle("gaia:sky1", location , molangVariables);
+    Gaia.spawnParticle("gaia:sky2", location , molangVariables);
+    Gaia.spawnParticle("gaia:sky_side1", location , molangVariables);
+    Gaia.spawnParticle("gaia:sky_side2", location , molangVariables);
+    Gaia.spawnParticle("gaia:sky_side3", location , molangVariables);
+    Gaia.spawnParticle("gaia:sky_side4", location , molangVariables);
+    Gaia.spawnParticle("gaia:gaia_planet", location , molangVariables);
+  }
+}
+// Return the class and method
+SkyboxRenderer;
