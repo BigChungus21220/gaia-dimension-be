21114e5e46149db79426632cd9d4d23d6e31e698
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2024-06-05 17:27:35 +0700

    Update GeyserPush.js

--- GaiaDimensions_BP/scripts/blocks/GeyserPush.js
+++ GaiaDimensions_BP/scripts/blocks/GeyserPush.js
@@ -1,19 +1,21 @@
 import { vec3 } from '../Vec3.js';
 import { world } from "@minecraft/server";
 import { push_entities } from "./Geyser.js";
+import { delay } from '../utils.js';
 
 world.beforeEvents.worldInitialize.subscribe(eventData => {
     eventData.blockTypeRegistry.registerCustomComponent('gaia:geyser', {
         async OnStepOn(e) {
-            let { block, player } = e;
+            let { block } = e;
+            let dimension = block.dimension;
             let spawn_pos = vec3(block.location).add(vec3(0.5, 1.1, 0.5)).toObject();
-            dimension.getPlayers().forEach((e) => { e.playSound("geyser.blast", { location: spawn_pos }); });
-            await new Promise(resolve => setTimeout(resolve, 10));
-            push_entities(dimension, spawn_pos, 120); // start blasting entities
+            dimension.getPlayers().forEach((e) => { e.playSound("geyser.blast", { location: spawn_pos }) });
+            await delay(10);
+            push_entities(dimension, spawn_pos, 120); //start blasting entities
             dimension.spawnParticle("gaia:geyser_pre_steam", spawn_pos);
-            await new Promise(resolve => setTimeout(resolve, 20));
+            await delay(20);
             dimension.spawnParticle("gaia:geyser_steam", spawn_pos);
             dimension.spawnParticle("gaia:geyser_blast", spawn_pos);
+    
         }
-    });
-});
+    })});
