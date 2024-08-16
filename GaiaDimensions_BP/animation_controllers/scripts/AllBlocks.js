a6a4c04ca91506218dd2a47335230b035b480bc1
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2024-08-16 15:48:21 +0700

    lol

--- GaiaDimensions_BP/animation_controllers/scripts/AllBlocks.js
+++
@@ -1,4 +0,0 @@
-import "./blocks/VanillaSlab.js";
-import "./blocks/VanillaLog.js";
-import "./blocks/GaiaGeyser.js"
-import "./blocks/PortalBlock.js"
--- GaiaDimensions_BP/animation_controllers/scripts/blocks/PortalBlock.js
+++
@@ -1,9 +0,0 @@
-import {world} from "@minecraft/server";
-
-world.beforeEvents.worldInitialize.subscribe(({blockTypeRegistry}) => {
-    blockTypeRegistry.registerCustomComponent("gaia:gaia_portal", {
-        onTick({block}) {
-           block.dimension.spawnParticle('gaia:portal',block.location)
-        },
-    });
-});
