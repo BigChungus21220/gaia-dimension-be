4f22371aa48351724e2827374c8e0474fcf42f02
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-02-02 20:33:01 +0700

    update

---
+++ GaiaDimensions_BP/scripts/world/Gaia.js
@@ -0,0 +1,17 @@
+const biomes = [
+    "mineral_river",
+    "volcanic_lands",
+    "shining_grove",
+    "smoldering_bog",
+    "static_wasteland",
+    "green_agate_jungle",
+    "crystal_plains",
+    "goldstone_lands",
+    "mutant_agate_wildwood",
+    "purple_agate_swamp",
+    "blue_agate_taiga",
+    "pink_agate_forest",
+    "salt_dunes",
+    "fossil_woodland",
+    "mineral_resevoir"
+]
