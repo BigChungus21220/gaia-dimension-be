4e9faaf99f7eb2d7c7dc5a731a9be6c6fd232ac5
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-02-03 22:05:28 +0700

    useless files GONE

--- GaiaDimensions_BP/scripts/registration/ModBlocks.js
+++ GaiaDimensions_BP/scripts/registration/ModBlocks.js
@@ -1,5 +1,40 @@
-//all the logs
-import "../blocks/LogRegistry.js"
+import { GAIA_LOG } from "../blocks/ModLogs"
+import { GAIA_SLAB } from "../blocks/ModSlabs"
 
-//slab registry
-import "../blocks/SlabRegistry.js"
+
+GAIA_SLAB.register("gaia:jade_brick_slab");
+GAIA_SLAB.register("gaia:cracked_jade_brick_slab");
+GAIA_SLAB.register("gaia:crusted_jade_brick_slab");
+GAIA_SLAB.register("gaia:copal_brick_slab");
+GAIA_SLAB.register("gaia:cracked_copal_brick_slab");
+GAIA_SLAB.register("gaia:crusted_copal_brick_slab");
+GAIA_SLAB.register("gaia:jet_brick_slab");
+GAIA_SLAB.register("gaia:cracked_jet_brick_slab");
+GAIA_SLAB.register("gaia:crusted_jet_brick_slab");
+GAIA_SLAB.register("gaia:amethyst_brick_slab");
+GAIA_SLAB.register("gaia:cracked_amethyst_brick_slab");
+GAIA_SLAB.register("gaia:crusted_amethyst_brick_slab");
+GAIA_SLAB.register("gaia:malachite_brick_slab");
+GAIA_SLAB.register("gaia:malachite_cracked_brick_slab");
+GAIA_SLAB.register("gaia:malachite_crusted_brick_slab");
+GAIA_SLAB.register("gaia:malachite_tile_slab");
+GAIA_SLAB.register("gaia:pink_agate_tile_slab");
+GAIA_SLAB.register("gaia:blue_agate_tile_slab");
+GAIA_SLAB.register("gaia:green_agate_tile_slab");
+GAIA_SLAB.register("gaia:purple_agate_tile_slab");
+GAIA_SLAB.register("gaia:fossilized_tile_slab");
+GAIA_SLAB.register("gaia:corrupted_tile_slab");
+GAIA_SLAB.register("gaia:burnt_agate_tile_slab");
+GAIA_SLAB.register("gaia:fire_agate_tile_slab");
+GAIA_SLAB.register("gaia:aura_tile_slab");
+GAIA_SLAB.register("gaia:golden_tile_slab");
+
+//register
+GAIA_LOG.register("gaia:aura_log")
+GAIA_LOG.register("gaia:blue_agate_log")
+GAIA_LOG.register("gaia:pink_agate_log")
+GAIA_LOG.register("gaia:purple_agate_log")
+GAIA_LOG.register("gaia:green_agate_log")
+GAIA_LOG.register("gaia:fossilized_log")
+GAIA_LOG.register("gaia:burned_agate_log")
+GAIA_LOG.register("gaia:burning_log")
