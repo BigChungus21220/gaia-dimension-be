4d08aa461dff3a08ec6de15d965ba4a9845c3d30
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-02-26 20:25:50 +0700

    ez

--- GaiaDimensions_BP/scripts/GaiaDimensionMod.js
+++ GaiaDimensions_BP/scripts/GaiaDimensionMod.js
@@ -1,12 +1,13 @@
-i//mport './asm/mixins.js';
-//import "./Vec3.js";
+import './asm/mixins.js';
+import "./Vec3.js";
 import './registration/ModBlocks.js';
+import "/world/DimensionRegistry.js"
 import './GaiaTeleporter.js';
 //import './tickEvents.js'
 import './Ignition.js';
 //import "./fluids/fluids.js";
-//import "./world/Biome.js";
-//import "./world/Fog.js";
+import "./world/Biome.js";
+import "./world/Fog.js";
 import "./world/TerrainPatching.js";
 //import './client/GaiaSkyRenderer.js';
 import "./entity/MalachiteGuard.js"
