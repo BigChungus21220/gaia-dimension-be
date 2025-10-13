40c886f84e2ff88fad88655db9a0b6d574bd71fb
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-10-13 22:45:35 +0700

    making way

--- GaiaDimensions_BP/scripts/blocks/PortalBlock.js
+++
@@ -1,9 +0,0 @@
-import {world} from "@minecraft/server";
-
-world.beforeEvents.worldInitialize.subscribe(({blockComponentRegistry}) => {
-    blockComponentRegistry.registerCustomComponent("gaia:gaia_portal", {
-        onTick({block}) {
-           block.dimension.spawnParticle('gaia:portal',block.location)
-        },
-    });
-});
--- GaiaDimensions_BP/scripts/world/DimensionRegistry.js
+++
@@ -1,7 +0,0 @@
-import { ModDimension } from "./ModDimension";
-
-
-ModDimension.register('gaia_dimension', {
-    range: { start: { x: 100000, z: 100000 }, end: { x: 400000, z: 400000 } },
-    inheritance: the_end
-});
