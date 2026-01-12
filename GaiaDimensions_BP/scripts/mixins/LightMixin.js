fe7bce8ba78ed3f0945432579803c3d61243a56b
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2026-01-12 15:23:51 +0700

    geyser + light + crates 2.0

--- GaiaDimensions_BP/scripts/mixins/LightMixin.js
+++ GaiaDimensions_BP/scripts/mixins/LightMixin.js
@@ -1,5 +1,22 @@
-import { world, BlockPermutation } from "@minecraft/server";
+import { world, system, BlockPermutation } from "@minecraft/server";
 import { DimensionSystem } from "../world/Gaia.js";
+
+let lightBlockPermutation;
+system.run(() => {
+    try {
+        lightBlockPermutation = BlockPermutation.resolve("minecraft:light_block", { "minecraft:block_light_level": 15 });
+    } catch (e) {}
+});
+
+function placeLight(dimension, location) {
+    if (!lightBlockPermutation) return;
+    try {
+        const block = dimension.getBlock(location);
+        if (block && block.isAir) {
+            block.setPermutation(lightBlockPermutation);
+        }
+    } catch (e) {}
+}
 
 export function initializeLightMixin() {
     world.afterEvents.playerPlaceBlock.subscribe((event) => {
@@ -7,6 +24,19 @@ export function initializeLightMixin() {
         const dimId = dimension.id;
         let stateVal = 0; // Default / Gaia
 
+        // --- Perpetual Glow System ---
+        // Place light sources around the newly placed block if in Gaia
+        if (player && DimensionSystem.isInGaia(player)) {
+             const { x, y, z } = block.location;
+             placeLight(dimension, { x: x + 1, y: y, z: z });
+             placeLight(dimension, { x: x - 1, y: y, z: z });
+             placeLight(dimension, { x: x, y: y + 1, z: z });
+             placeLight(dimension, { x: x, y: y - 1, z: z });
+             placeLight(dimension, { x: x, y: y, z: z + 1 });
+             placeLight(dimension, { x: x, y: y, z: z - 1 });
+        }
+
+        // --- Custom Block State Logic ---
         // Check if the block has the dimension permutation property
         try {
             const currentState = block.permutation.getState("gaiadimension:perm_dim");
@@ -32,6 +62,14 @@ export function initializeLightMixin() {
             }
         } catch (e) {
             // Block doesn't have the state or other error
+        }
+    });
+
+    world.afterEvents.playerBreakBlock.subscribe((event) => {
+        const { player, block, dimension } = event;
+        if (player && DimensionSystem.isInGaia(player)) {
+             // Fill the broken spot with light
+             placeLight(dimension, block.location);
         }
     });
 }
